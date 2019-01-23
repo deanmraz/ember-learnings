3713e225d8fb0d43fb8359c510a9eb3c08b69da5
Dean M <deanm>
2019-01-22 21:01:09 -0800

    Add jest image snapshot example

---
+++ integration/Button.test.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+describe('Button', () => {
+  test('visually looks correct', async () => {
+    // APIs from jest-puppeteer
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('http://localhost:9001/iframe.html?selectedKind=Demo&selectedStory=button&full=0&addons=1&stories=1&panelRight=0&addonPanel=storybook%2Factions%2Factions-panel');
+    try {
+      const image = await page.screenshot({fullPage: true});
+      // API from jest-image-snapshot
+      expect(image).toMatchImageSnapshot();
+    } catch (e) {
+      console.error(e);
+    }
+
+    await page.close();
+    await browser.close();
+
+  });
+});
